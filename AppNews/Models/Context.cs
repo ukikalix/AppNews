6acf7613f6282d453f424ba3f5ae49b9b0e89ec2
Ivan Lara <uki@live.co.uk>
2018-12-07 09:52:11 -0600

    Creando modelo y conexion a DB

---
+++ AppNews/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AppNews.Models
+{
+    public class Context: DbContext
+    {
+
+        public Context()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<News> TheNews { get; set; }
+    }
+}
---
+++ AppNews/Models/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AppNews.Models
+{
+    //Fecha de Publicación
+    //Título
+    //Contenido(todo la noticia en particular debe permitir texto enriquecido (HTML))
+    //Imagen(URL de la imagen)
+
+    public class News
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public DateTime Post { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Content { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
