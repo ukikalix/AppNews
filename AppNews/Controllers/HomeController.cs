44fdd7d45fa3db6c3e3fbd43ba9757e0332479e6
Ivan Lara <uki@live.co.uk>
2018-12-08 10:40:04 -0600

    Agregando Servicio para consumir API

--- AppNews/Controllers/HomeController.cs
+++ AppNews/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppNews.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace AppNews.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<News> _news = Migrations.Services.GetJson("http://localhost:51221/api/News").Result.ToList();
+            return View(_news);
         }
 
         public ActionResult About()
---
+++ AppNews/Migrations/Services.cs
@@ -0,0 +1,19 @@
+﻿using AppNews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppNews.Migrations
+{
+    public class Services
+    {
+        public static async System.Threading.Tasks.Task<List<News>> GetJson(string uri)
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<News>>(await client.GetStringAsync(uri));
+            }
+        }
+    }
+}
