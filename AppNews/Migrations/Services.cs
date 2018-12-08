1b4c14b00c3f7fbe5dbfb6841bfc49814d56da23
Ivan Lara <uki@live.co.uk>
2018-12-08 11:28:56 -0600

    Creando servicio para consumir API, y mostrar con AngularJS en Inicio

--- AppNews/Migrations/Services.cs
+++ AppNews/Migrations/Services.cs
@@ -2,17 +2,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace AppNews.Migrations
 {
     public class Services
     {
-        public static async System.Threading.Tasks.Task<List<News>> GetJson(string uri)
+        public static async Task<List<News>> GetJson(string uri)
         {
-            using (var client = new System.Net.Http.HttpClient())
+            List<News> _list = new List<News>();
+            using (var client = new HttpClient())
             {
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<News>>(await client.GetStringAsync(uri));
+                try
+                {
+                    client.BaseAddress = new Uri(uri);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var result = await client.GetStringAsync(uri);
+                        _list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<News>>(result);
+                        return _list;
+                    } else
+                        return new List<News>();
+                }
+                catch (Exception)
+                {
+                    return new List<News>();
+                }
             }
         }
     }
